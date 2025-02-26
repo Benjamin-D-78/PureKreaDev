b0b520503e97787f9a59961618bdf987b6ef3c98
Benjamin DESMONET <desmonet.pro@laposte.net>
2025-02-26 11:37:56 +0100

    ajout axios

--- front/src/utils/Constantes.jsx
+++ front/src/utils/Constantes.jsx
@@ -1,44 +1,44 @@
 export const URL = {
 
     // USER
-    USER_CONNEXION: "http://localhost:8000/api/user/connexion",
-    USER_INSCRIPTION: "http://localhost:8000/api/user/inscription",
-    USER_BY_ID: "http://localhost:8000/api/user/obtenir",
-    USER_ALL: "http://localhost:8000/api/user/all",
-    USER_UPDATE: "http://localhost:8000/api/user/update",
-    USER_DELETE: "http://localhost:8000/api/user/delete",
+    USER_CONNEXION: "/api/user/connexion",
+    USER_INSCRIPTION: "/api/user/inscription",
+    USER_BY_ID: "/api/user/obtenir",
+    USER_ALL: "/api/user/all",
+    USER_UPDATE: "/api/user/update",
+    USER_DELETE: "/api/user/delete",
 
     // EMAIL
-    EMAIL_VERIFICATION: "http://localhost:8000/api/user/verification",
-    EMAIL_VERIFICATION_BIS: "http://localhost:8000/api/user/verification/bis",
+    EMAIL_VERIFICATION: "/api/user/verification",
+    EMAIL_VERIFICATION_BIS: "/api/user/verification/bis",
 
     // ITEM
-    ITEM_CREATION: "http://localhost:8000/api/item/creation",
-    ITEM_BY_ID: "http://localhost:8000/api/item/obtenir",
-    ITEM_ALL: "http://localhost:8000/api/item/all",
-    ITEM_UPDATE: "http://localhost:8000/api/item/update",
-    ITEM_DELETE: "http://localhost:8000/api/item/delete",
+    ITEM_CREATION: "/api/item/creation",
+    ITEM_BY_ID: "/api/item/obtenir",
+    ITEM_ALL: "/api/item/all",
+    ITEM_UPDATE: "/api/item/update",
+    ITEM_DELETE: "/api/item/delete",
 
     // COMMANDE
-    COMMANDE_CREATION: "http://localhost:8000/api/commande/creation",
-    COMMANDE_BY_USER: "http://localhost:8000/api/commande/obtenir/commandes",
-    COMMANDE_BY_ID: "http://localhost:8000/api/commande/obtenir",
-    COMMANDE_ALL: "http://localhost:8000/api/commande/all",
-    COMMANDE_UPDATE: "http://localhost:8000/api/commande/update",
-    COMMANDE_DELETE: "http://localhost:8000/api/commande/delete",
+    COMMANDE_CREATION: "/api/commande/creation",
+    COMMANDE_BY_USER: "/api/commande/obtenir/commandes",
+    COMMANDE_BY_ID: "/api/commande/obtenir",
+    COMMANDE_ALL: "/api/commande/all",
+    COMMANDE_UPDATE: "/api/commande/update",
+    COMMANDE_DELETE: "/api/commande/delete",
 
     // MESSAGES
-    MESSAGE_CREATION: "http://localhost:8000/api/contact/creation",
-    MESSAGE_ALL: "http://localhost:8000/api/contact/all",
-    MESSAGE_UPDATE: "http://localhost:8000/api/contact/update",
-    MESSAGE_DELETE: "http://localhost:8000/api/contact/delete",
+    MESSAGE_CREATION: "/api/contact/creation",
+    MESSAGE_ALL: "/api/contact/all",
+    MESSAGE_UPDATE: "/api/contact/update",
+    MESSAGE_DELETE: "/api/contact/delete",
 
     // NEWSLETTER
-    ABONNE_CREATION: "http://localhost:8000/api/abonnement/creation",
-    ABONNE_ALL: "http://localhost:8000/api/abonnement/all",
-    ABONNE_UPDATE: "http://localhost:8000/api/abonnement/update",
-    ABONNE_DELETE: "http://localhost:8000/api/abonnement/delete",
+    ABONNE_CREATION: "/api/abonnement/creation",
+    ABONNE_ALL: "/api/abonnement/all",
+    ABONNE_UPDATE: "/api/abonnement/update",
+    ABONNE_DELETE: "/api/abonnement/delete",
 
     // PAIEMENT
-    CHARGEMENT: "http://localhost:8000/api/paiement/chargement"
+    CHARGEMENT: "/api/paiement/chargement"
 }
---
+++ front/src/utils/axiosInstance.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+// constSi tu es en local tu utilise cette URL, si tu es en ligne tu utilise l'autre URL
+const API_URL = process.env.NODE_ENV === "development" ? "http://localhost:8000" : "https://pure-krea-dev-muz5.vercel.app"
+
+const axiosInstance = axios.create({
+    baseURL: API_URL,
+    withCredentials: true,
+    headers: {"Content-Type": "application/json"}
+})
+
+export default axiosInstance;
