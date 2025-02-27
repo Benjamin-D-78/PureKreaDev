a466f5c961b4f54fe4e4687367f6a3e9a3dd2192
Benjamin DESMONET <desmonet.pro@laposte.net>
2025-02-27 23:15:41 +0100

    annulation *

--- back/server.js
+++ back/server.js
@@ -29,7 +29,7 @@ mongoose
 
 // MIDDLEWARE
 app.use(cors({
-    origin: ["*"],
+    origin: ["http://localhost:3000", "https://pure-krea-benjamind.vercel.app"],
     methods: 'GET, POST, DELETE, PUT',
     credentials: true,
     allowedHeaders: ['Content-Type','Authorization','Cookie'],
