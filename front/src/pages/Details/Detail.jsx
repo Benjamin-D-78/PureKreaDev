64df44082121cc4c1f94fe3950f7c4eeb76222bd
Benjamin DESMONET <desmonet.pro@laposte.net>
2025-02-27 12:32:47 +0100

    page détails

--- front/src/pages/Details/Detail.jsx
+++ front/src/pages/Details/Detail.jsx
@@ -47,9 +47,10 @@ const Details = () => {
             if (URL.ITEM_BY_ID) {
                 try {
                     const { data, status } = await axiosInstance.get(`${URL.ITEM_BY_ID}/${id}`)
-                    if (Array.isArray(data)) {
+                    // if (Array.isArray(data)) {
+                        console.log(data)
                         setItem(data)
-                    };
+                    // };
                 } catch (error) {
                     setError("Erreur lors de la réception des données", error)
                 }
