2a70617c0f6f8101b9ee45b241d4872f12153bf1
Benjamin DESMONET <desmonet.pro@laposte.net>
2025-02-28 10:25:21 +0100

    test resend 2

--- back/services/resend.js
+++ back/services/resend.js
@@ -1,30 +1,30 @@
-// import { Resend } from 'resend';
-// import { env } from '../config/index.js';
+import { Resend } from 'resend';
+import { env } from '../config/index.js';
 
-// const resend = new Resend(env.RESEND_API_KEY);
+const resend = new Resend(env.RESEND_API_KEY);
 
-// export const sendEmail = async (user, verifieToken) => {
-//     const verificationURL = process.env.NODE_ENV === 'development' 
-//         ? `http://localhost:3000/verification/${verifieToken}` 
-//         : `https://pure-krea-benjamind.vercel.app/verification/${verifieToken}`;
+export const sendEmail = async (user, verifieToken) => {
+    const verificationURL = process.env.NODE_ENV === 'development' 
+        ? `http://localhost:3000/verification/${verifieToken}` 
+        : `https://pure-krea-benjamind.vercel.app/verification/${verifieToken}`;
     
-//     const htmlContent = `
-//         <p>Bonjour ${user.name},</p>
-//         <p>Merci de vous être inscrit ! Cliquez sur le lien ci-dessous pour vérifier votre email.</p>
-//         <a href="${verificationURL}">${verificationURL}</a>
-//     `;
+    const htmlContent = `
+        <p>Bonjour ${user.name},</p>
+        <p>Merci de vous être inscrit ! Cliquez sur le lien ci-dessous pour vérifier votre email.</p>
+        <a href="${verificationURL}">${verificationURL}</a>
+    `;
 
-//     try {
-//         // On envoie l'email
-//         const response = await resend.emails.send({
-//             from: 'desmonet.idf@gmail.com',
-//             to: user.email,
-//             subject: 'Vérification de votre email',
-//             html: htmlContent,
-//         });
+    try {
+        // On envoie l'email
+        const response = await resend.emails.send({
+            from: 'desmonet.idf@gmail.com',
+            to: user.email,
+            subject: 'Vérification de votre email',
+            html: htmlContent,
+        });
 
-//         console.log('Email envoyé avec succès:', response);
-//     } catch (error) {
-//         console.error('Erreur lors de l\'envoi de l\'email:', error);
-//     }
-// };
+        console.log('Email envoyé avec succès:', response);
+    } catch (error) {
+        console.error('Erreur lors de l\'envoi de l\'email:', error);
+    }
+};
