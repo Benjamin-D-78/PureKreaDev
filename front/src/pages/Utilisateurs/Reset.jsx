ccda7a2f3ad76bd6e991d0299c1b8d9339252449
Benjamin DESMONET <desmonet.pro@laposte.net>
2025-03-05 18:53:02 +0100

    test token 4

--- front/src/pages/Utilisateurs/Reset.jsx
+++ front/src/pages/Utilisateurs/Reset.jsx
@@ -16,6 +16,7 @@ import { URL } from "../../utils/Constantes";
 const Reset = () => {
 
     const [email, setEmail] = useState("");
+    const [responseMessage, setReponseMessage] = useState("");
 
     const [user, setUser] = useState({
         email: ""
@@ -53,6 +54,7 @@ const Reset = () => {
         if (URL.USER_RESET) {
             try {
                 const response = await axiosInstance.post(URL.USER_RESET, { email })
+                setReponseMessage(response.data.message)
                 toast.success("Email envoyé avec succès.")
                 setEmail("");
 
