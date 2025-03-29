3e32c9a0e5136d656d735a097ed50712ebb1d24a
Benjamin DESMONET <desmonet.pro@laposte.net>
2025-03-29 14:19:25 +0100

    test recaptcha

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -113,12 +113,30 @@ export default function Footer() {
         setTimeout(() => {
           window.location.reload();
         }, 3000);
-        
+
       }
     } else {
       toast.error("Veuillez réessayer plus tard.", { autoClose: 3000 })
     }
   }
+
+  useEffect(() => {
+    // On créer le script dans le DOM
+    const script = document.createElement('script');
+    // On indique l'url du fichier JS qu'on veut utiliser
+    script.src = 'https://www.google.com/recaptcha/api.js?render='+RECAPTCHA_PUBLIC_KEY;
+    // Chargement asynchrone : le navigateur peut continuer de télécharger d'autres ressources pendant que le script est chargé = amélioration des performances de la page.
+    script.async = true;
+    // Le script ne sera exécuté qu'une fois que tout le DOM sera chargé
+    script.defer = true;
+    // On ajoute le script au corps "body" de la page
+    document.body.appendChild(script);
+
+    return () => {
+      // On nettoie tout effet secondaire laissé par le composant une fois qu'on en a plus besoin.
+      document.body.removeChild(script);
+    };
+  }, []);
 
 
   return (
