dbbe6d56f783b65dc33967541213ce81c16c10c4
Benjamin DESMONET <desmonet.pro@laposte.net>
2025-03-29 12:42:55 +0100

    test bouton 3

--- front/src/components/PrenezRendezVous/prenezRendezVous.jsx
+++ front/src/components/PrenezRendezVous/prenezRendezVous.jsx
@@ -5,9 +5,9 @@ const PrenezRendezVous = () => {
     return (
         <div className={calltoaction.contientCallToAction}>
             <p className={calltoaction.pCallToAction}>Laissez-vous tenter par le sur-mesure...</p>
-            <div className={calltoaction.divBtnCallToAction}>
-                <Link to="/rendez-vous"><button className={calltoaction.btnCallToAction}>Prenez rendez-vous</button></Link>
-            </div>
+            {/* <div className={calltoaction.divBtnCallToAction}> */}
+                <Link to="/rendez-vous">Prenez rendez-vous</Link>
+            {/* </div> */}
         </div>
     )
 }
