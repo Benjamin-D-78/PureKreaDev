5bf965e60e46beba62b8b6a2bf31077da4dfc37e
Benjamin DESMONET <desmonet.pro@laposte.net>
2025-03-30 13:21:46 +0200

    test centralisation footer

--- front/src/utils/recaptcha.js
+++ front/src/utils/recaptcha.js
@@ -1,49 +1,46 @@
 // import { useEffect, useState, useRef } from "react";
 
 // Clé publique
+import { useState, useEffect, useRef } from 'react';
+
 export const RECAPTCHA_PUBLIC_KEY = "6Le0SuYqAAAAAHfMcbGHjP1Ggqpsa_ynGieVDhqL"
+// import { RECAPTCHA_PUBLIC_KEY } from '../../utils/recaptcha';
 
-// const Recaptcha = () => {
+const useRecaptcha = () => {
+  const refRecaptcha = useRef(null);
+  const [recaptchaToken, setRecaptchaToken] = useState(null);
 
-//     const refRecaptcha = useRef(null)
-//     const [recaptchaToken, setRecaptchaToken] = useState(null);
+  const handleRecaptcha = (value) => {
+    setRecaptchaToken(value);
+  };
 
-//     useEffect(() => {
-//         // On créer le script dans le DOM
-//         const script = document.createElement('script');
-//         // On indique l'url du fichier JS qu'on veut utiliser
-//         script.src = 'https://www.google.com/recaptcha/api.js?render=' + RECAPTCHA_PUBLIC_KEY;
-//         // Chargement asynchrone : le navigateur peut continuer de télécharger d'autres ressources pendant que le script est chargé = amélioration des performances de la page.
-//         script.async = true;
-//         // Le script ne sera exécuté qu'une fois que tout le DOM sera chargé
-//         script.defer = true;
-//         // On ajoute le script au corps "body" de la page
-//         document.body.appendChild(script);
+  const resetRecaptcha = () => {
+    setRecaptchaToken(null);
+    if (refRecaptcha.current) {
+      refRecaptcha.current.reset();
+    }
+  };
 
-//         return () => {
-//             // On nettoie tout effet secondaire laissé par le composant une fois qu'on en a plus besoin.
-//             document.body.removeChild(script);
-//         };
-//     }, []);
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `https://www.google.com/recaptcha/api.js?render=${RECAPTCHA_PUBLIC_KEY}`;
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
 
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
-//     // on appelle handleRecaptcha lorsque l'utilisateur clique sur le bouton.
-//     //   Lorsque l'utilisateur clique sur le bouton, value est égal au token qui est généré lors du clic.
-//     const handleRecaptcha = (value) => {
-//         setRecaptchaToken(value);
+  return {
+    refRecaptcha,
+    recaptchaToken,
+    handleRecaptcha,
+    resetRecaptcha,
+  };
+};
 
-//     };
+export default useRecaptcha;
 
-//     const resetRecaptcha = (value) => {
-//         setRecaptchaToken(null);
-//         if (refRecaptcha.current) {
-//             refRecaptcha.current.reset()
-//         }
-//     }
-
-
-//     return { refRecaptcha, recaptchaToken, handleRecaptcha, resetRecaptcha }
-// }
-
-// export default Recaptcha
 
