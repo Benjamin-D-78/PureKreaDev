e0c2b388700964e1552731571f685c0411be0623
Benjamin <desmonet.pro@laposte.net>
2025-05-08 14:48:39 +0200

    test form

--- front/src/pages/PrendreRendez-Vous/Testi.jsx
+++ front/src/pages/PrendreRendez-Vous/Testi.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Testi = () => {
-  return (
-    <div>Test Coucou</div>
-  )
+
+    const [user, setUser] = useState({
+        prenom: "",
+        nom: ""
+    })
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setUser(prev => ({ ...prev, [name]: value }))
+    }
+
+    return (
+        <div>
+            <p>{user.prenom}</p>
+            <p>{user.nom}</p>
+
+            <input
+                onChange={handleChange}
+                type="text"
+                name='prenom'/>
+            <input
+                onChange={handleChange}
+                type="text"
+                name='nom'/>
+        </div>
+    )
 }
 
 export default Testi
